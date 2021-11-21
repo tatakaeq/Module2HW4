3b3acdbfe0c68a899a1d3d09ad77170e32d8d5ca
Andrew Kara <a.kara701@gmail.com>
2021-11-21 17:34:16 +0200

    Initial commit

---
+++ Module2HW4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module2HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
